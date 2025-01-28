f57f6c85c53db50d15e3a12aa486f50082f2ea32
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-29 00:03:01 +0200

    route

--- main/views.py
+++ main/views.py
@@ -9,3 +9,15 @@ def home():
 @views.route("/book", methods=["GET", "POST"])
 def book_seat():
     return render_template("home/book.html")
+
+@views.route("/homestead")
+def homestead():
+    return render_template("resources/homestead.html")
+
+@views.route("/tetraton")
+def tetraton():
+    return "hi"
+
+@views.route("/spaceship")
+def spaceship():
+    return "yhup"
