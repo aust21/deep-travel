482f0398f17009f45dd835b82bd011cfb7f0bee0
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-29 01:11:33 +0200

    app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask_migrate import Migrate
+
+from main import create_app
+from main import db
+
+app = create_app()
+migrate = Migrate(app, db)
+
+
+if __name__ == '__main__':
+    app.run()
