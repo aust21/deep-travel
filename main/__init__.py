7e982008a4b0839479815a923fa3f175090672fa
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-29 01:50:04 +0200

    --no-edit

--- main/__init__.py
+++ main/__init__.py
@@ -7,7 +7,6 @@ import os
 DB_NAME = "database.db"
 db = SQLAlchemy()
 
-
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config['SECRET_KEY'] = os.urandom(24)
