2f4aa1ff0af898eb1341fbaada33683c72e1253a
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-28 22:58:11 +0200

    create book html

--- main/views.py
+++ main/views.py
@@ -5,3 +5,7 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def home():
     return render_template("home/home.html")
+
+@views.route("/book", methods=["GET", "POST"])
+def book_seat():
+    return render_template("home/book.html")
