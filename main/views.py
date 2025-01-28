fff9be43a9c1d2beb16d709ded08b5f4391170c6
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-29 01:04:17 +0200

    make confirm pages

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, redirect, url_for
 
 views = Blueprint("views", __name__)
 
@@ -8,7 +8,21 @@ def home():
 
 @views.route("/book", methods=["GET", "POST"])
 def book_seat():
+    if request.method == "POST":
+        # Get form data
+        name = request.form.get("name")
+        email = request.form.get("email")
+        destination = request.form.get("destination")
+        departure = request.form.get("departure")
+
+        # Validate form fields
+        if all([name, email, destination, departure]):
+            return redirect(url_for("views.thanks"))
+        else:
+            return redirect(url_for("views.error"))
+    
     return render_template("home/book.html")
+
 
 @views.route("/homestead")
 def homestead():
@@ -21,3 +35,11 @@ def tetraton():
 @views.route("/spaceship")
 def spaceship():
     return "yhup"
+
+@views.route("/thank-you")
+def thanks():
+    return render_template("resources/thanks.html")
+
+@views.route("/error")
+def error():
+    return render_template("resources/error.html")
